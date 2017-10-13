6992e2ba0a20ad3ff5e63d71e8b38161ee12bf6f
Patrick Glynn <pjglynn57@gmail.com>
2017-10-13 15:23:57 -0600

    random color mouseover functionality

---
+++ board.js
@@ -0,0 +1,38 @@
+var rowArr = [];
+var sqArr = [];
+
+for (let i=0; i<4; i++) {
+  var contain = document.getElementsByClassName("contain")[0];
+  var row = document.createElement("div");
+  contain.appendChild(row);
+  row.className="row";
+  row.id=`${i}`
+  rowArr.push(row);
+
+  for (let j=0; j<20; j++) {
+    var sq = document.createElement("div");
+    row.appendChild(sq);
+    sq.className="square";
+    sq.id=`${i}${j}`
+    sqArr.push(sq);
+    sq.counter = 0;
+  }
+}
+
+// console.log(rowArr);
+console.log(sqArr);
+
+for (let square of sqArr) {
+  square.addEventListener("mouseover", function () {
+    square.style.backgroundColor = randomColor();
+  }, false);
+}
+
+function randomColor() {
+var letters = '0123456789ABCDEF';
+var color = '#';
+for (var i = 0; i < 6; i++) {
+ color += letters[Math.floor(Math.random() * 16)];
+ }
+return color;
+}
