74e57890536f3b8e76844bb30f853b2cc3a4018f
Patrick Glynn <pjglynn57@gmail.com>
2017-10-15 11:45:43 -0600

    working on selectColor functionality

--- board.js
+++ board.js
@@ -1,38 +1,73 @@
-var rowArr = [];
 var sqArr = [];
+var tileArr = [];
 
 for (let i=0; i<100; i++) {
-  var contain = document.getElementsByClassName("contain")[0];
+  var board = document.getElementsByClassName("board")[0];
   var row = document.createElement("div");
-  contain.appendChild(row);
+  board.appendChild(row);
   row.className="row";
   row.id=`${i}`
-  rowArr.push(row);
 
-  for (let j=0; j<100; j++) {
+  for (let j=0; j<50; j++) {
     var sq = document.createElement("div");
     row.appendChild(sq);
     sq.className="square";
     sq.id=`${i}${j}`
     sqArr.push(sq);
-    sq.counter = 0;
   }
 }
 
-// console.log(rowArr);
-console.log(sqArr);
+for (let c=0; c<25; c++) {
+  var left = document.getElementsByClassName("left")[0];
+  var leftCol = document.createElement("div");
+  leftCol.className="col";
+  left.appendChild(leftCol);
+
+  var right = document.getElementsByClassName("right")[0];
+  var rightCol = document.createElement("div");
+  rightCol.className="col";
+  right.appendChild(rightCol);
+
+  for (let t=0; t<10; t++) {
+    var leftTile = document.createElement("div");
+    leftTile.className="tile";
+    leftCol.prepend(leftTile);
+    leftTile.style.backgroundColor = randomColor();
+    var rightTile = document.createElement("div");
+    rightTile.className="tile";
+    rightCol.prepend(rightTile);
+    rightTile.style.backgroundColor = randomColor();
+    tileArr.push(leftTile, rightTile)
+  }
+}
+
+
+// console.log(sqArr);
 
 for (let square of sqArr) {
   square.addEventListener("mouseover", function () {
-    square.style.backgroundColor = randomColor();
+    square.style.backgroundColor = currentColor;
   }, false);
 }
 
+var currentColor;
+
+for (var tile of tileArr) {
+  tile.addEventListener("click", selectColor());
+}
+
+
 function randomColor() {
-var letters = '0123456789ABCDEF';
-var color = '#';
-for (var i = 0; i < 6; i++) {
- color += letters[Math.floor(Math.random() * 16)];
- }
-return color;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+   color += letters[Math.floor(Math.random() * 16)];
+   }
+  return color;
+}
+
+function selectColor() {
+  currentColor = tile.style.backgroundColor;
+  console.log(currentColor);
+  return currentColor;
 }
